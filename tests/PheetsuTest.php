9661ca454b086a392bdd54c5a2e4d42f63e1cffb
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2017-08-24 07:29:35 +0900

    init with koriym/php-skeleton

---
+++ tests/PheetsuTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Ttskch\Pheetsu;
+
+use PHPUnit\Framework\TestCase;
+
+class PheetsuTest extends TestCase
+{
+    /**
+     * @var Pheetsu
+     */
+    protected $skeleton;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->skeleton = new Pheetsu;
+    }
+
+    public function testNew()
+    {
+        $actual = $this->skeleton;
+        $this->assertInstanceOf('\Ttskch\Pheetsu\Pheetsu', $actual);
+    }
+}
